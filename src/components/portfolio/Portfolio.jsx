54acc32a1f71909da477d173d17e6846a1bb6350
Anurag236 <anuragkumar3260@gmail.com>
2024-06-28 16:50:03 +0530

    done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,7 @@ import IMG1 from "../../assets/pp1.png";
 import IMG2 from "../../assets/pp2.png";
 import IMG3 from "../../assets/pp3.png";
 import IMG4 from "../../assets/dr.png";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+
 
 const data = [
   {
